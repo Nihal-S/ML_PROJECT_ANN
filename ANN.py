a35d2e65d9656eab184770bcbf915434df5e1648
nihal-s <nihalsreenivasu@gmail.com>
2019-10-23 15:17:51 +0530

    comparing algo random forest and ann are the best and must be optimized

--- ANN.py
+++ ANN.py
@@ -192,9 +192,9 @@ minmax = dataset_minmax(dataset)
 normalize_dataset(dataset, minmax)
 # evaluate algorithm
 n_folds = 5
-l_rate = 0.5
-n_epoch = 1000
-n_hidden = 7
+l_rate = 0.3
+n_epoch = 500
+n_hidden = 5
 scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
 print('Scores: %s' % scores)
 print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
